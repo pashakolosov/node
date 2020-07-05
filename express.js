23528bf90528c8f0b16bad9daaef33107d1ade11
pasha <pasha.colocov@gmail.com>
2020-07-06 00:17:26 +0300

    'server_log'

--- express.js
+++ express.js
@@ -11,11 +11,16 @@ app.use((req, res, next) => {
 });
 app.use((req, res, next) => {
     console.log('middleware2', req.url);
-    res.send('middleware2');
+    next();
 });
+app.use('/home', (req, res, next) => {
+    console.log('about')
+    res.send('<h1>Эта функция выполняется только при запросе /about</h1>')
+})
 
 app.get('/', (req, res) => {
     console.log('home');
-    res.end('<h1>Hello</h1>')
-})
+    res.end('<h1>Hello</h1>');
+});
+
 app.listen(3000, () => console.log('server has been started on 3000 port...'))
