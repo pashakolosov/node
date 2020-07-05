e3773f0d69e20ab0b64129445a07b5a5f279d401
pasha <pasha.colocov@gmail.com>
2020-07-05 23:53:41 +0300

    'app_use'

--- express.js
+++ express.js
@@ -1,21 +1,21 @@
 const express = require('express');
+const { response } = require('express');
 
 const app = express();
 
 
+app.use((req, res, next) => {
+
+    console.log('middleware1', req.url);
+    next();
+});
+app.use((req, res, next) => {
+    console.log('middleware2', req.url);
+    res.send('middleware2');
+});
+
 app.get('/', (req, res) => {
-    res.end('<h1>Hello express</h1>');
-});
-
-
-app.get('/home', (req, res) => {
-    res.end('<h1>Hello Home</h1>');
-});
-
-
-app.get('/about', (req, res) => {
-    res.end('<h1>About</h1>');
-});
-
-
-app.listen(3000, () => console.log('server has been started...'));
+    console.log('home');
+    res.end('<h1>Hello</h1>')
+})
+app.listen(3000, () => console.log('server has been started on 3000 port...'))
