b38668eaea6399d91a648c5860ee293be502be81
pasha <pasha.colocov@gmail.com>
2020-07-06 18:58:38 +0300

    'fixed'

--- fs/fix.js
+++ fs/fix.js
@@ -2,52 +2,50 @@ const fs = require('fs');
 const path = require('path');
 
 fs.mkdir(
-    path.join(__dirname, 'kek'),
+    path.join(__dirname, 'new'),
     err => {
         if (err) throw err;
-        
+
         console.log('mkdir');
 
         fs.writeFile(
-            path.join(__dirname, 'kek', 'kek.txt'),
-            'hello Kek',
+            path.join(__dirname, 'new', 'kek.txt'),
+            'hello kek',
             err => {
                 if (err) throw err;
 
                 console.log('write');
 
                 fs.appendFile(
-                    path.join(__dirname, 'kek', 'kek.txt'),
-                    '\n append kek',
+                    path.join(__dirname, 'new', 'kek.txt'),
+                    'append file kek',
                     err => {
                         if (err) throw err;
 
                         console.log('append');
 
                         fs.readFile(
-                            path.join(__dirname, 'kek', 'kek.txt'),
+                            path.join(__dirname, 'new', 'kek.txt'),
                             'utf8',
                             (err, data) => {
                                 if (err) throw err;
 
-                                console.log('read');
-                                console.log(data)
+                                console.log(data);
 
                                 fs.rename(
-                                    path.join(__dirname, 'kek', 'kek.txt'),
-                                    path.join(__dirname, 'kek', 'renamed.txt'),
+                                    path.join(__dirname, 'new', 'kek.txt'),
+                                    path.join(__dirname, 'new', 'rename.txt'),
                                     err => {
                                         if (err) throw err;
 
-                                        console.log('renamed')
-
-                                        console.log()
+                                        console.log('rename');
                                     }
                                 )
                             }
                         )
                     }
                 )
+
             }
         )
     }
