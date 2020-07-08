d514aa5d6cc040f9fffe10f9f2cd0b4ca3ccd20c
pasha <pasha.colocov@gmail.com>
2020-07-08 10:57:35 +0300

    'upd'

--- http/http.js
+++ http/http.js
@@ -1,21 +1,57 @@
-const http = require('http')
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const server = http.createServer((req, res) => {
+    console.log(req.url)
   if (req.method === 'GET') {
-    res.writeHead(200, {
-      'Content-Type': 'text/html'
-    })
-    res.end(`
-      <h1>Form</h1>
-      <form method="post" action="/">
-        <input name="title" type="text" />
-        <button type="submit">Send</button>
-      </form>
-    `) 
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
+
+    switch (req.url) {
+        case '/':
+            fs.readFile(
+                path.join(__dirname, 'views', 'index.html'),
+                'utf-8',
+                (err, content) => {
+                    if (err) throw err
+
+                    res.end(content)
+                }
+            )
+            break
+        case '/favicon.ico':
+            res.writeHead(200, {'Content-Type': 'image/x-icon'} )
+            res.end()
+            console.log('favicon requested')
+            break
+        case '/about':
+            fs.readFile(
+                path.join(__dirname, 'views', 'about.html'),
+                'utf-8',
+                (err, content) => {
+                    if (err) throw err
+
+                    res.end(content)
+                }
+            )
+            break
+        case '/api/users':
+            res.writeHead(200, {'Content-Type': 'text/json'})
+
+            const users = [
+                {name: 'Pasha', age:20},
+                {name: 'Vasya', age: 22}
+            ]
+
+            res.end(JSON.stringify(users))
+            break
+    }
+
+   
 } else if (req.method === 'POST') {
     const body = []
     res.writeHead(200, {
-      'Content-Type': 'text/html; charset=ascii'
+      'Content-Type': 'text/html; charset=utf8'
     })
 
     req.on('data', data => {
